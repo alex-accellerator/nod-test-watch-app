87ec4840cbf403ceeda29883a241fde1b0b67116
namxkim <namxkim@gmail.com>
2015-06-13 14:52:45 -0700

    Add additional nod image assets. Add SendingNodInterfaceController

---
+++ NodWatchTestApp WatchKit Extension/SendingNodInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  SendingNodInterfaceController.h
+//  NodWatchTestApp
+//
+//  Created by Nam Kim on 6/13/15.
+//  Copyright © 2015 Nam Kim. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface SendingNodInterfaceController : WKInterfaceController
+
+@end
---
+++ NodWatchTestApp WatchKit Extension/SendingNodInterfaceController.m
@@ -0,0 +1,36 @@
+//
+//  SendingNodInterfaceController.m
+//  NodWatchTestApp
+//
+//  Created by Nam Kim on 6/13/15.
+//  Copyright © 2015 Nam Kim. All rights reserved.
+//
+
+#import "SendingNodInterfaceController.h"
+
+@interface SendingNodInterfaceController ()
+
+@end
+
+@implementation SendingNodInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
